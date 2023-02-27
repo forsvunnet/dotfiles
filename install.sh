de236f71cbf43515a342f8ab9978fbd31156959b
Eivin Landa <eivin@cove.no>
2023-02-27 11:10:13 +0100

    Added stuff

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+
+brew install fzf the_silver_searcher
+
